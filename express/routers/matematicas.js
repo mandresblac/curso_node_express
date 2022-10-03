df98d3e05aba4230776b121aa33e50e7c5722415
manuel blanco <mandresblac@gmail.com>
2022-10-03 16:25:17 -0500

    commit 6

--- express/routers/matematicas.js
+++ express/routers/matematicas.js
@@ -4,6 +4,9 @@ const {matematicas} = require("../datos/cursos.js").infoCursos;
 
 const routerMatematicas = express.Router(); //Para crear un router
 
+//Middleware
+routerMatematicas.use(express.json());
+
 //Creamos otra ruta para obtener los cursos de matemáticas solamente con el mismo método get de http
 routerMatematicas.get("/", (req, res) => {
   //Enviamos una respuesta con res.send()
@@ -24,6 +27,13 @@ routerMatematicas.get("/:tema", (req, res) => {
 
   //El estado por defecto es 200 OK
   res.send(JSON.stringify(resultados));
+});
+
+//Método post de http
+routerMatematicas.post("/", (req, res) => {
+  let cursoNuevo = req.body; //Extraemos el cuerpo de la solicitud con req.body
+  matematicas.push(cursoNuevo);
+  res.json(matematicas); //Enviamos  el nuevo array de cursos de programación al cliente
 });
 
 
--- express/routers/programacion.js
+++ express/routers/programacion.js
@@ -5,10 +5,16 @@ const {programacion} = require('../datos/cursos.js').infoCursos;
 const routerProgramacion = express.Router(); //Para crear un router
 
 
+//Middleware para procesar el cuerpo de las solicitudes en formato .json y poder trabajar con ese cuerpo de la solicitud en nuestro código como la propiedad body
+//La funciones Middleware se ejecutan después de recibir una solicitud, pero antes de enviar una respuesta
+//Los Middleware tienen acceso al objeto de la solicitud, al objeto de la respuesta y a next(), que es una función que se llama para ejecutar el proximo Middleware
+routerProgramacion.use(express.json());
+
+
 //Creamos otra ruta para obtener los cursos de programación solamente con el mismo método get de http
 routerProgramacion.get('/', (req, res) => {
   //Enviamos una respuesta con res.send() solo de la información de los cursos que correspondan a programación
-  res.send(JSON.stringify(programacion));
+  res.json(programacion);
 });
 
 
@@ -27,13 +33,12 @@ routerProgramacion.get('/:lenguaje', (req, res) => {
   //PARÁMETROS QUERY
   //Vamos a implementar una funcionalidad para poder ordenar los resultados de acuerdo al número de vistas
   if (req.query.ordenar === 'vistas') {
-    return res.send(JSON.stringify(resultados.sort((a, b) => b.vistas - a.vistas)));
+    return res.send(resultados.sort((a, b) => b.vistas - a.vistas));
   };
 
   //El estado por defecto es 200 OK
-  res.send(JSON.stringify(resultados));
+  res.json(resultados);
 });
-
 
 
 //URL con dos parámetros
@@ -46,12 +51,67 @@ routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
 
   if (resultados.length === 0) {
     //Con status() asignamos el estado de la respuesta http, que en este caso es 404
-    return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+    return res.status(204).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
   };
 
   //El estado por defecto es 200 OK
-  res.send(JSON.stringify(resultados));
+  res.json(resultados);
 });
 
 
+//Método post de http
+routerProgramacion.post("/", (req, res) => {
+  //Extraemos el cuerpo de la solicitud para poder incluir un curso nuevo en el arreglo de cursos de programación
+  let cursoNuevo = req.body; //Extraemos el cuerpo de la solicitud con req.body
+  programacion.push(cursoNuevo);
+  res.json(programacion); //Enviamos  el nuevo array de cursos de programación al cliente
+});
+
+//Método PUT de http
+routerProgramacion.put("/:id", (req, res) => {
+  const cursoActualizado = req.body;
+  const id = req.params.id; //Extraemos el "id" del curso
+
+  //Para encontrar el indice del curso en el array 
+  //El método findIndex() nos permite encontrar el indice de un elemento en un array  en base a un criterio
+  const indice = programacion.findIndex(curso => curso.id == id);
+
+  if (indice >= 0) {
+    programacion[indice] = cursoActualizado;
+  }
+
+  res.send(programacion);
+});
+
+//Método PATCH de http
+routerProgramacion.patch("/:id", (req, res) => {
+  const infoActualizada = req.body;
+  const id = req.params.id;
+
+  const indice = programacion.findIndex(curso => curso.id == id);
+
+  if (indice >= 0) {
+    const cursoAModificar = programacion[indice];
+    //El método assign() permite modificar solo algunas propiedades de un objeto
+    Object.assign(cursoAModificar, infoActualizada);
+  }
+
+  res.json(programacion);
+});
+
+
+//Método DELETE de http
+routerProgramacion.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  const indice = programacion.findIndex(curso => curso.id == id);
+
+  if (indice >= 0) {
+    //Par elimina run elemento especifico de un array podemos utilizar el método .splice()
+    programacion.splice(indice, 1);
+  }
+
+  res.json(programacion);
+});
+
 module.exports = routerProgramacion;
+  
